0e61de7dfb3b7401821c86a25f028fc8131b60fa
rar4 <66429342+rar4@users.noreply.github.com>
2024-11-07 11:45:41 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+
+
+const int side = 10;
+
+
+
+class Entity
+
+{
+
+public:
+
+
+
+protected:
+
+int Hp;
+
+int Damage;
+
+int Defence;
+
+}
+
+
+
+Entity::Entity()
+
+{
+
+}
+
+
+
+Entity::~Entity()
+
+{
+
+}
+
+
+
+class Plane
+
+{
+
+public:
+
+
+
+
+
+protected:
+
+char plane[side][side];
+
+
+
+};
+
+
+
+Plane::Plane()
+
+{
+
+}
+
+
+
+Plane::~Plane()
+
+{
+
+}
+
+
+
+
+
+int main() {
+
+}
